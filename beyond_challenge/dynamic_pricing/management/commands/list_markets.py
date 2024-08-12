bc9ec169c24d5f72ade9de8ec7409665474f3f62
Walter A. Alves <walter.arruda.alves@gmail.com>
2024-08-12 14:32:49 -0300

    chore: Add data migration to initial markets and their rules
    - Add migration to add Paris, Lisbon and San Francisco markets and their rules;
    - Add a sample command to check the existing markets and rules;

---
+++ beyond_challenge/dynamic_pricing/management/commands/list_markets.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+
+from dynamic_pricing.models.market import Market
+from dynamic_pricing.models.rule import Rule
+
+class Command(BaseCommand):
+    help = "Displays the market and its rules in the system"
+
+    def handle(self, *args, **kwargs):
+        for market in Market.objects.all():
+            print(market.label)
+            for rule in market.rules.all():
+                print(f"\t\t{rule.day_of_week}, {rule.multiplier}")
+                print()
